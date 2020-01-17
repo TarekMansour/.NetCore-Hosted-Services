4047758e1f186a3600db2b8c576a348085cb1433
Tarek Mansour <tmansour@mantu.com>
2020-01-17 15:13:57 +0100

    Add application insights to gather background service events

--- Sample.Services/ConsumeJobHostedService.cs
+++ Sample.Services/ConsumeJobHostedService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Threading;
@@ -9,39 +11,51 @@ namespace Sample.Services
     public class ConsumeJobHostedService : IHostedService, IDisposable
     {
         public IServiceProvider Services { get; }
+        private TelemetryClient _telemetryClient;
         private Timer _timer;
 
-        public ConsumeJobHostedService(IServiceProvider services)
+        public ConsumeJobHostedService(IServiceProvider services, TelemetryClient telemetryClient)
         {
             Services = services;
+            _telemetryClient = telemetryClient;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
+            _telemetryClient.TrackEvent("Job Hosted Service is running.");
             try
             {
                 _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
                 return Task.CompletedTask;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                _telemetryClient.TrackTrace($"{ex} catched while executing the BackgroundService task!");
                 throw;
             }
         }
 
         private void DoWork(object state)
-        { 
-            using (var scope = Services.CreateScope())
+        {
+            _telemetryClient.TrackEvent("Job Hosted Service is working.");
+
+            using (_telemetryClient.StartOperation<RequestTelemetry>("operation"))
             {
-                var jobService =
-                    scope.ServiceProvider
-                    .GetRequiredService<IJobService>();
-                jobService.DoWork();
+                using (var scope = Services.CreateScope())
+                {
+                    var jobService =
+                        scope.ServiceProvider
+                            .GetRequiredService<IJobService>();
+
+                    jobService.DoWork();
+                }
+                _telemetryClient.TrackEvent("Task completed.");
             }
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
+            _telemetryClient.TrackEvent("Job Hosted Service is stopping.");
             _timer?.Change(Timeout.Infinite, 0);
             return Task.CompletedTask;
         }
